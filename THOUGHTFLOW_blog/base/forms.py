8276aa0e9cf082185e39f179d249b1fd5e4bacc2
Manalhub <manalrahali24@gmail.com>
2024-05-26 21:21:36 +0100

    Enhance code structure and add comments to forms file

--- THOUGHTFLOW_blog/base/forms.py
+++ THOUGHTFLOW_blog/base/forms.py
@@ -1,39 +1,36 @@
 from django import forms
 from .models import Comment, Post
 
+# Form for creating and updating posts
 class POSTForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super(forms.ModelForm, self).__init__(*args, **kwargs)
-        self.fields['title'].widget.attrs.update(
-            {'class': 'mt-1 p-2 border w-full rounded-md focus:outline-none focus:ring-emeral-400 focus:border-emerald-300' }
-        )
-        self.fields['body'].widget.attrs.update(
-            {'class': 'mt-1 p-2 border w-full rounded-md focus:outline-none focus:ring-emeral-400 focus:border-emerald-300' }
-        )
-        self.fields['image'].widget.attrs.update(
-            {'class': 'mt-1 p-2 border w-full rounded-md focus:outline-none focus:ring-emeral-400 focus:border-emerald-300' }
-        )
-        
+        super(POSTForm, self).__init__(*args, **kwargs)
+        for field_name in ['title', 'body', 'image']:
+            self.fields[field_name].widget.attrs.update(
+                {'class': 'mt-1 p-2 border w-full rounded-md focus:outline-none focus:ring-emerald-400 focus:border-emerald-300'}
+            )
 
     class Meta:
         model = Post
         fields = ['title', 'body', 'image']
         labels = {
-            'about': ('Write something about you'),
-            'avatar': ('Profile Photo'),
-            'talks_about': ('Topics you talks about')
+            'title': 'Title',
+            'body': 'Body',
+            'image': 'Image',
         }
 
+# Form for creating and updating comments
 class COMForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        super(COMForm, self).__init__(*args, **kwargs)
         self.fields['body'].widget.attrs.update(
-            {'class': 'mt-1 p-2 border w-full rounded-md focus:outline-none focus:ring-emeral-400 focus:border-emerald-300' }
+            {'class': 'mt-1 p-2 border w-full rounded-md focus:outline-none focus:ring-emerald-400 focus:border-emerald-300'}
         )
-        
+
     class Meta:
         model = Comment
         fields = ['body']
         labels = {
-            'text': ('Comment'),
+            'body': 'Comment',
         }
+
