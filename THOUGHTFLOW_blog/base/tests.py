9c856d776befd55b8d3c8bad3229da353cf29108
Manalhub <manalrahali24@gmail.com>
2024-05-30 10:39:15 +0100

    add some tests

--- THOUGHTFLOW_blog/base/tests.py
+++ THOUGHTFLOW_blog/base/tests.py
@@ -1,3 +1,158 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# test_models.py
+from django.contrib.auth.models import User
+
+from profiles.models import Userprofile
+from .models import Post, Category, Comment
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='12345')
+        self.post = Post.objects.create(
+            title='Test Post',
+            author=self.user,
+            body='This is a test post',
+            category='Test Category'
+        )
+
+    def test_post_creation(self):
+        self.assertTrue(isinstance(self.post, Post))
+        self.assertEqual(self.post.__str__(), self.post.title)
+    
+    
+    def test_post_get_absolute_url(self):
+        url = self.post.get_absolute_url()
+        self.assertEqual(url, f"/read_post/{self.post.id}/{self.post.slug}/")
+
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(title='Test Category')
+
+    def test_category_creation(self):
+        self.assertTrue(isinstance(self.category, Category))
+        self.assertEqual(self.category.__str__(), self.category.title)
+
+class CommentModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='12345')
+        self.post = Post.objects.create(
+            title='Test Post',
+            author=self.user,
+            body='This is a test post',
+            category='Test Category'
+        )
+        self.comment = Comment.objects.create(
+            author='Commenter',
+            comment_p=self.post,
+            body='This is a test comment'
+        )
+
+    def test_comment_creation(self):
+        self.assertTrue(isinstance(self.comment, Comment))
+        self.assertEqual(self.comment.__str__(), f"Comment by {self.comment.author} on {self.comment.comment_p}")
+
+# test_views.py
+from django.test import Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post, Category, Comment, User
+
+class ViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Userprofile.objects.create(person=self.user)
+        self.category = Category.objects.create(title='Test Category')
+        self.post = Post.objects.create(
+            title='Test Post',
+            slug='test-post',
+            author=self.user,
+            body='This is a test post',
+            category='Test Category'
+        )
+        self.comment = Comment.objects.create(
+            author='Commenter',
+            comment_p=self.post,
+            body='This is a test comment'
+        )
+
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_post_categories_view(self):
+        response = self.client.get(reverse('post_categories', args=[self.category.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_create_post_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('create_post'), {
+            'title': 'Another Test Post',
+            'body': 'This is another test post',
+            'category': self.category.id
+        })
+        self.assertEqual(response.status_code, 302)  # Redirects after successful post creation
+
+    def test_read_post_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('read_post', args=[self.post.id, self.post.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post.html')
+
+    def test_update_post_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('update_post', args=[self.post.id]), {
+            'title': 'Updated Test Post',
+            'body': 'This is an updated test post'
+        })
+        self.assertEqual(response.status_code, 302)  # Redirects after successful post update
+
+    def test_delete_post_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('delete_post', args=[self.post.id]))
+        self.assertEqual(response.status_code, 302)  # Redirects after successful post deletion
+
+    def test_delete_comment_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('delete_comment', args=[self.comment.id]))
+        self.assertEqual(response.status_code, 302)  # Redirects after successful comment deletion
+
+    def test_update_comment_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('update_comment', args=[self.comment.id]), {
+            'body': 'Updated comment body'
+        })
+        self.assertEqual(response.status_code, 302)  # Redirects after successful comment update
+
+# test_forms.py
+from .forms import POSTForm, COMForm
+from .models import Post, Comment
+
+class PostFormTest(TestCase):
+    def test_post_form_valid(self):
+        form = POSTForm(data={
+            'title': 'Test Title',
+            'body': 'Test body',
+            'image': None
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_post_form_invalid(self):
+        form = POSTForm(data={})
+        self.assertFalse(form.is_valid())
+
+class CommentFormTest(TestCase):
+    def test_comment_form_valid(self):
+        form = COMForm(data={
+            'body': 'Test comment body'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_comment_form_invalid(self):
+        form = COMForm(data={})
+        self.assertFalse(form.is_valid())
--- THOUGHTFLOW_blog/profiles/tests.py
+++ THOUGHTFLOW_blog/profiles/tests.py
@@ -1,3 +1,104 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+from django.test import Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Userprofile
+from base.models import Post
+from .forms import UserPForm
+
+# test_models.py
+class UserprofileModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Userprofile.objects.create(
+            person=self.user,
+            about='This is a test user profile',
+            talks_about='Testing',
+            avatar='profile_photos/test_avatar.jpg'
+        )
+
+    def test_userprofile_creation(self):
+        self.assertTrue(isinstance(self.profile, Userprofile))
+        self.assertEqual(self.profile.__str__(), f'Profile of {self.profile.person.get_full_name()} last logged in at {self.profile.person.last_login}')
+
+
+# test_views.py
+class ViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Userprofile.objects.create(
+            person=self.user,
+            about='This is a test user profile',
+            talks_about='Testing',
+            avatar='profile_photos/test_avatar.jpg'
+        )
+        self.post = Post.objects.create(
+            title='Test Post',
+            author=self.user,
+            body='This is a test post',
+            category='Test Category'
+        )
+
+    def test_user_login_view(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'testuser',
+            'password': '12345'
+        })
+        self.assertEqual(response.status_code, 302)  # Redirects to home on successful login
+
+    def test_user_logout_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.status_code, 302)  # Redirects to home on logout
+
+    def test_register_user_view(self):
+        response = self.client.post(reverse('register'), {
+            'first-name': 'first-name',
+            'last-name': 'last-name',
+            'username': 'username',
+            'password': 'password',
+            'password_confirm': 'passwordr',
+            'email': 'email',
+            'about': 'about',
+            'talks_about': 'talks_about'
+        })
+        self.assertEqual(response.status_code, 200) 
+
+def test_user_profile_view(self):
+        response = self.client.get(reverse('user_profile', args=[self.user.username, self.user.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profile.html')
+
+def test_update_profile_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('update_profile', args=[self.user.username, self.user.id]), {
+            'about': 'Updated about',
+            'talks_about': 'Updated topics'
+        })
+        self.assertEqual(response.status_code, 302)  # Redirects to profile on successful update
+
+# Form Tests
+class UserPFormTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Userprofile.objects.create(
+            person=self.user,
+            about='This is a test user profile',
+            talks_about='Testing'
+        )
+
+    def test_userp_form_valid(self):
+        form = UserPForm(data={
+            'about': 'Updated about me',
+            'talks_about': 'Updated topics',
+        }, instance=self.profile)
+        self.assertTrue(form.is_valid())
+
+    def test_userp_form_invalid(self):
+        form = UserPForm(data={}, instance=self.profile)
+        self.assertFalse(form.is_valid())
